726ae58f124fe306792710ad3c294611fd3d6e59
nira <prunusnira@gmail.com>
2023-01-11 20:25:24 +0900

    패턴 컨테이너 데이터 추가

--- src/controller/patternController.ts
+++ src/controller/patternController.ts
@@ -1,21 +1,25 @@
-import express from 'express'
-import Filter from '../tool/Filter'
+import express from "express";
+import Filter from "../tool/Filter";
 
 const PatternController = () => {
-    const router = express.Router()
+    const router = express.Router();
+
+    // rank for patterns
+    router.get("/pattern/:ver/:order/:page", async (req, res) => {
+        const { ver, order, page } = req.params;
+    });
 
     // Pattern List
-    router.get('/list/:ver/:order/:page/:hot', async (req, res) => {
-        const version = req.params.ver // version 숫자
-        const order = req.params.order
-        const page = req.params.page
-        const hot = req.params.hot // hot, other, all
+    router.get("/list/:ver/:order/:page/:hot", async (req, res) => {
+        const version = req.params.ver; // version 숫자
+        const order = req.params.order;
+        const page = req.params.page;
+        const hot = req.params.hot; // hot, other, all
 
-        const verarr = Filter.filterVersion(version)
-        
-    })
+        const verarr = Filter.filterVersion(version);
+    });
 
-    return router
-}
+    return router;
+};
 
-export default PatternController
+export default PatternController;
